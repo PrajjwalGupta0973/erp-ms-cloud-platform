798eee21d569eaf398e8bcda5903d4e7d7ea579e
Prajjwal Gupta <prajjwalgupta0973@gmail.com>
2025-09-28 16:17:16 +0530

    added more remaining resources

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,7 +4,7 @@ locals {
   region = "us-east-1"
   zone1 = "us-east-1a"
   zone2 = "us-east-1b"
-  eks_cluster_name = "eks-cluster-${local.env}"
+  eks_cluster_name = "erp"
   eks_version = "1.29"
 }
 
