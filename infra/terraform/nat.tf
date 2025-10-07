f795eb48b28957d9cbfccb1725923fc6758e30c1
Prajjwal Gupta <prajjwalgupta0973@gmail.com>
2025-10-07 10:28:35 +0530

    tested with sample ingress

--- infra/terraform/nat.tf
+++ infra/terraform/nat.tf
@@ -2,9 +2,10 @@ resource "aws_eip" "nat" {
   domain = "vpc"
 
   tags = {
-    Name = "${local.env}-nat-eib"
+    Name = "${local.env}-nat"
   }
 }
+
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public_zone1.id
