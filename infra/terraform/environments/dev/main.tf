33d38f246b426aa825e623f068084b972a8d8397
Prajjwal Gupta <prajjwalgupta0973@gmail.com>
2025-10-10 22:28:41 +0530

    till helm provider

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -63,12 +63,26 @@ module "eks_admin_user" {
   kubernetes_admin_group_name = var.kubernetes_admin_group_name
 }
 
-data "aws_eks_cluster_auth" "eks_data" {
+data "aws_eks_cluster" "eks" {
+  name = module.eks_cluster.eks_cluster_name
+}
+
+data "aws_eks_cluster_auth" "eks" {
   name = module.eks_cluster.eks_cluster_name
 }
 
 provider "kubernetes" {
-  host                   = module.eks_cluster.eks_cluster_name
-  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_ca)
-  token                  = data.aws_eks_cluster_auth.eks_data.token
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
+
+provider "helm" {
+
+  kubernetes = {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
+
